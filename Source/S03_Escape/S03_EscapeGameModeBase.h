8a35e88ff3baded40aed8536787a237bef631c12
Joshua <jnokoi@gmail.com>
2017-06-28 11:46:16 -0700

    init commit

---
+++ Source/S03_Escape/S03_EscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "S03_EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class S03_ESCAPE_API AS03_EscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
